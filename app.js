fbf0f668d4a088c26141db97ee6c585b96c8e907
Consuelo Leal <consuelo.leal@outlook.com>
2019-04-01 12:25:09 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,23 @@
+let cipherMesagge;
+let userName;
+let offsetNumber;
+
+function welcome(){
+	userName
+	document.getElementByiId('myName').value 
+
+}
+
+function clean(){
+    document.getElementByiId
+
+
+}
+
+function offset(){
+
+}
+function 
+//veriables que guardan el mensaje a cifrar, el nombre del usuario y
+//la cantidad de dezplazamiento
+//IMPLEMENTAR 
